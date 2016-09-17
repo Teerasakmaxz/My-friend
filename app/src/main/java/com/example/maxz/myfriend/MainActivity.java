fc334fd45ecab46edfc611c3ee2c780f206a7446
Teerasak <maxz_drum@icloud.com>
2016-09-17 11:21:50 +0700

    InInet SignUp

--- app/src/main/java/com/example/maxz/myfriend/MainActivity.java
+++ app/src/main/java/com/example/maxz/myfriend/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.maxz.myfriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    //ประกาศตัวแปร
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }//main method
+
+    public void click_signUPMain(View view) {
+
+        startActivity(new Intent(MainActivity.this,Sign_upActivity.class));
+
     }
-}
+
+
+}//main class
---
+++ app/src/main/java/com/example/maxz/myfriend/Sign_upActivity.java
@@ -0,0 +1,13 @@
+package com.example.maxz.myfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sign_upActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }//main method 2
+}//main class 2
